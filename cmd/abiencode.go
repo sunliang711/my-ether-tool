6309b46bb495457d269abb2774f541c717e89c50
sunliang711 <sunliang711@163.com>
2023-07-29 18:40:49 +0800

    abi encode: separate function

--- cmd/abiencode.go
+++ cmd/abiencode.go
@@ -4,15 +4,18 @@ Copyright © 2023 NAME HERE <EMAIL ADDRESS>
 package cmd
 
 import (
-	"encoding/hex"
+	// "encoding/hex"
 	"fmt"
-	"math/big"
+	"my-ether-tool/transaction"
 	"my-ether-tool/utils"
-	"strings"
-
-	"github.com/ethereum/go-ethereum/accounts/abi"
-	"github.com/ethereum/go-ethereum/common"
-	"github.com/ethereum/go-ethereum/crypto"
+	// "math/big"
+	// "my-ether-tool/transaction"
+	// "my-ether-tool/utils"
+	"os"
+	// "strings"
+	// "github.com/ethereum/go-ethereum/accounts/abi"
+	// "github.com/ethereum/go-ethereum/common"
+	// "github.com/ethereum/go-ethereum/crypto"
 	"github.com/spf13/cobra"
 )
 
@@ -50,82 +53,14 @@ func init() {
 	abiStr = abiencodeCmd.Flags().String("abi", "", "abi string, eg: transfer(address,uint256)")
 	abiArgs = abiencodeCmd.Flags().StringArray("args", nil, "arguments of abi(--args 0x... --args 200)")
 
-	var err error
-	uint256Type, err = abi.NewType("uint256", "", nil)
-	if err != nil {
-		panic("create uint256 failed")
-	}
-	bytes32Type, err = abi.NewType("bytes32", "", nil)
-	if err != nil {
-		panic("create bytes32 failed")
-	}
-	addressType, err = abi.NewType("address", "", nil)
-	if err != nil {
-		panic("create address failed")
-	}
 }
-
-var (
-	uint256Type abi.Type
-	bytes32Type abi.Type
-	addressType abi.Type
-)
 
 func abiEncode(cmd *cobra.Command, args []string) {
 	utils.ExitWithMsgWhen(*abiStr == "", "need --abi <abi_string>")
-
-	// selector is the first 4 bytes of hash of abiStr
-	selector := crypto.Keccak256Hash([]byte(*abiStr)).Bytes()[:4]
-
-	result := selector
-
-	fields := strings.FieldsFunc(*abiStr, func(r rune) bool {
-		return r == '(' || r == ')'
-	})
-
-	fmt.Printf("fields: %v\n", fields)
-
-	arguments := abi.Arguments{}
-	argumentsValue := []any{}
-
-	if len(fields) == 2 {
-		argTypes := fields[1]
-
-		types := strings.Split(argTypes, ",")
-
-		utils.ExitWithMsgWhen(len(types) != len(*abiArgs), "number of arguments  not match with abi string\n")
-
-		for i := range types {
-			abiArg := (*abiArgs)[i]
-			fmt.Printf("> abi arg: %s\n", abiArg)
-
-			switch types[i] {
-			case "address":
-				fmt.Printf("> address type\n")
-				arguments = append(arguments, abi.Argument{Type: addressType})
-				argumentsValue = append(argumentsValue, common.HexToAddress(abiArg))
-			case "uint256":
-				fmt.Printf("> uint256 type\n")
-				arguments = append(arguments, abi.Argument{Type: uint256Type})
-				v, ok := new(big.Int).SetString(abiArg, 10)
-				utils.ExitWithMsgWhen(!ok, "invalid uint256 type argument\n")
-				argumentsValue = append(argumentsValue, v)
-			case "bytes32":
-				fmt.Printf("> bytes32 type\n")
-				arguments = append(arguments, abi.Argument{Type: bytes32Type})
-				decoded, err := hex.DecodeString(abiArg)
-				utils.ExitWithMsgWhen(err != nil, "invalid bytes32 type argument\n")
-				argumentsValue = append(argumentsValue, decoded)
-			// TODO: other types
-			default:
-				fmt.Printf("donot support type: %s", types[i])
-			}
-		}
-		packed, err := arguments.Pack(argumentsValue...)
-		utils.ExitWithMsgWhen(err != nil, "pack arguments error: %s\n", err)
-		result = append(result, packed...)
-
+	encoded, err := transaction.AbiEncode(*abiStr, *abiArgs)
+	if err != nil {
+		fmt.Fprintf(os.Stdout, "error: %s\n", err)
+		os.Exit(1)
 	}
-
-	fmt.Printf("result: 0x%s\n", hex.EncodeToString(result))
+	fmt.Printf("result: %s\n", encoded)
 }
---
+++ transaction/abi.go
@@ -0,0 +1,107 @@
+package transaction
+
+import (
+	"encoding/hex"
+	"errors"
+	"fmt"
+	"math/big"
+	"strings"
+
+	"github.com/ethereum/go-ethereum/accounts/abi"
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/crypto"
+)
+
+var (
+	uint256Type abi.Type
+	bytes32Type abi.Type
+	addressType abi.Type
+)
+
+func init() {
+
+	var err error
+	uint256Type, err = abi.NewType("uint256", "", nil)
+	if err != nil {
+		panic("create uint256 failed")
+	}
+	bytes32Type, err = abi.NewType("bytes32", "", nil)
+	if err != nil {
+		panic("create bytes32 failed")
+	}
+	addressType, err = abi.NewType("address", "", nil)
+	if err != nil {
+		panic("create address failed")
+	}
+}
+
+func AbiEncode(abiStr string, abiArgs []string) (encoded string, err error) {
+	// selector is the first 4 bytes of hash of abiStr
+	selector := crypto.Keccak256Hash([]byte(abiStr)).Bytes()[:4]
+
+	result := selector
+
+	fields := strings.FieldsFunc(abiStr, func(r rune) bool {
+		return r == '(' || r == ')'
+	})
+
+	arguments := abi.Arguments{}
+	argumentsValue := []any{}
+
+	if len(fields) == 2 {
+		argTypes := fields[1]
+
+		types := strings.Split(argTypes, ",")
+
+		if len(types) != len(abiArgs) {
+			err = errors.New("number of arguments  not match with abi string")
+			return
+
+		}
+
+		for i := range types {
+			abiArg := (abiArgs)[i]
+			fmt.Printf("> abi arg: %s\n", abiArg)
+
+			switch types[i] {
+			case "address":
+				fmt.Printf("> address type\n")
+				arguments = append(arguments, abi.Argument{Type: addressType})
+				argumentsValue = append(argumentsValue, common.HexToAddress(abiArg))
+			case "uint256":
+				fmt.Printf("> uint256 type\n")
+				arguments = append(arguments, abi.Argument{Type: uint256Type})
+				v, ok := new(big.Int).SetString(abiArg, 10)
+				if !ok {
+					err = errors.New("invalid uint256 type argument")
+					return
+				}
+				argumentsValue = append(argumentsValue, v)
+			case "bytes32":
+				var decoded []byte
+				fmt.Printf("> bytes32 type\n")
+				arguments = append(arguments, abi.Argument{Type: bytes32Type})
+				decoded, err = hex.DecodeString(abiArg)
+				if err != nil {
+					err = fmt.Errorf("invalid bytes32 type argument: %s", err)
+					return
+				}
+				argumentsValue = append(argumentsValue, decoded)
+			// TODO: other types
+			default:
+				err = fmt.Errorf("not supprt type: %s", types[i])
+				return
+			}
+		}
+		var packed []byte
+		packed, err = arguments.Pack(argumentsValue...)
+		if err != nil {
+			err = fmt.Errorf("pack arguments error: %s", err)
+			return
+		}
+		result = append(result, packed...)
+
+	}
+
+	return "0x" + hex.EncodeToString(result), nil
+}
