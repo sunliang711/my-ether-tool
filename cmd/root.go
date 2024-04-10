61fd11ae601ec01284ada50d643e8938fa3ae7b7
sunliang711 <sunliang711@163.com>
2024-04-10 22:01:44 +0800

    command message

--- cmd/root.go
+++ cmd/root.go
@@ -4,20 +4,16 @@ Copyright © 2023 NAME HERE <EMAIL ADDRESS>
 package cmd
 
 import (
-	"github.com/spf13/cobra"
 	"os"
+
+	"github.com/spf13/cobra"
 )
 
 // RootCmd represents the base command when called without any subcommands
 var RootCmd = &cobra.Command{
 	Use:   "my-ether-tool",
-	Short: "B brief description of your application",
-	Long: `B longer description that spans multiple lines and likely contains
-examples and usage of using your application. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short: "my ether tool",
+	Long:  `evm based blockchain client CLI tool`,
 	// Uncomment the following line if your bare application
 	// has an action associated with it:
 	// Run: func(cmd *cobra.Command, args []string) { },
